0fbd72c2053ffdc8aeedef9df494e72c7a6b4882
HashirAshraf <74363048+HashirAshraf@users.noreply.github.com>
2021-03-24 19:37:58 +0000

    Update JS.js

--- JS.js
+++ JS.js
@@ -36,14 +36,41 @@ function shuffleArray(deck)
 function displayCards(deck, cardNumber) {
     document.getElementById("deck").innerHTML = deck[cardNumber].Colour + deck[cardNumber].Value;
 }
-
 function pickCards(shuffledDeck) {
-    let playerTwoDeck[];
-
-    while (let i = 1; i < shuffledDeck.length; i++) {
-        if ()
-
-
+    let playerOneDeck = [];
+    let playerTwoDeck = [];
+    let i = 1
+    while (i < shuffledDeck.length) {
+        if (shuffledDeck[i - 1].Suit == shuffledDeck[i].Suit) {
+            if (shuffledDeck[i - 1].Value > shuffledDeck[i].Value) {
+                playerOneDeck.push({Suit: shuffledDeck[i - 1].Suit, Value: shuffledDeck[i - 1].Value});
+                playerOneDeck.push({Suit: shuffledDeck[i].Suit, Value: shuffledDeck[i].Value});
             }
+        } else {
+            playerTwoDeck.push({Suit: shuffledDeck[i - 1].Suit, Value: shuffledDeck[i - 1].Value});
+            playerTwoDeck.push({Suit: shuffledDeck[i].Suit, Value: shuffledDeck[i].Value});
+        }
+    }
+    if (playerOneDeck.length > playerTwoDeck.length) {
+        displayDeck("Player One", playerOneDeck);
+    }
+    else{
+        displayDeck("Player Two", playerTwoDeck);
+    }
+}
 
+function displayDeck(winner, deck) {
+    document.getElementById("winningPlayer").innerHTML = winner;
+    document.getElementById("noOfCards").innerHTML = deck.length;
+
+
+    let deckHTML = '<table><tr>'
+
+    for (let i = deck.length - 1; i < 0; i++) {
+        let card = deck[i].Suit + deck[i].Value;
+        let image = "img/" + deck[i].Suit + deck[i].Value + ".jpg";
+        deckHTML += `<td>${card}</td><td><img src=${image} width="40px" height="60px"></td>`;
+        deckHTML += '</tr></table>';
+        document.getElementById("winner").innerHTML = deckHTML;
+    }
 }
